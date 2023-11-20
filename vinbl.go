7e612aa8d67a343c2b4766ff001dfb26ba63af17
Anil-kumar9 <126747805+Anil-kumar9@users.noreply.github.com>
2023-11-20 12:56:59 +0530

    Update vinbl.go

--- vinbl.go
+++ vinbl.go
@@ -1,38 +1,29 @@
 package main
 
 import (
-	"io"
-	"os"
+	"encoding/json"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-type LD struct {
-	LogEnabled bool
-}
+var logger = logrus.New()
 
-func (l *LD) Levels() []logrus.Level {
-	return logrus.AllLevels
-}
-
-func (l *LD) Fire(entry *logrus.Entry) error {
-	if l.LogEnabled || entry.Data["vin"].(bool) {
-		entry.Logger.Out = os.Stdout
-	} else if entry.Level == logrus.ErrorLevel {
-		entry.Logger.Out = os.Stdout
-	} else {
-		entry.Logger.SetOutput(io.Discard)
+func Log(logMap map[string]interface{}, level logrus.Level) {
+	if viper.GetBool("VIN") || isVinEnabled(logMap["vin"].(string)) {
+		msg, err := json.Marshal(logMap)
+		if err != nil {
+			logger.Info(msg)
+		}
+	} else if level == logrus.ErrorLevel {
+		logMap["vin"] = "hashed"
+		msg, err := json.Marshal(logMap)
+		if err != nil {
+			logger.Error(msg)
+		}
 	}
-	return nil
 }
 
-func New() *logrus.Logger {
-	logger := logrus.New()
-	logEnabled := viper.GetBool("VIN")
-	ld := LD{
-		LogEnabled: logEnabled,
-	}
-	logger.AddHook(&ld)
-	return logger
+func isVinEnabled(vin string) bool {
+	return vin == "true"
 }
